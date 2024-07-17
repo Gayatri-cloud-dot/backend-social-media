81371f5be0b42c680f800652b4b8a0aa91826f56
GayatriRankawat <146867910+GayatriRankawat@users.noreply.github.com>
2024-07-17 14:40:31 +0530

    ..

--- config/database.js
+++ config/database.js
@@ -1,10 +1,8 @@
 const mongoose = require("mongoose");
 
-exports.connectDatabase=()=>{
-    mongoose
+exports.connectDatabase = () => {
+  mongoose
     .connect("mongodb://localhost:27017/SocialMedia")
-    .then ((con)=> console.log(`database connected :${con.connection.host}` ))
-    .catch((err)=>console.log(err));
+    .then((con) => console.log(`database connected :${con.connection.host}`))
+    .catch((err) => console.log(err));
 };
-
-
--- server.js
+++ server.js
@@ -1,9 +1,18 @@
-const app=require("./app");
+const app = require("./app");
 const { connectDatabase } = require("./config/database");
-const dotenv=require("dotenv");
-dotenv.config()
+const dotenv = require("dotenv");
+const cors = require("cors");
+dotenv.config();
 
 connectDatabase();
-app.listen(4000,()=>{
-console.log(`server is running on port ${4000}`);
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+app.listen(4000, () => {
+  console.log(`server is running on port ${4000}`);
 });
